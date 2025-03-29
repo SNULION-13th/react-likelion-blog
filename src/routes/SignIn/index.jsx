1c33460e9aad8f735d3262db2ab98160dc0bb0dd
jdnjsy <jdnjsyoo@snu.ac.kr>
2025-03-29 22:45:54 +0900

    first commit

--- src/routes/SignIn/index.jsx
+++ src/routes/SignIn/index.jsx
@@ -2,3 +2,16 @@
 export default function SignIn() {
   return <div>SignIn</div>;
 }
+
+// ⬇️추가
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+  CardFooter,
+} from "@/components/ui/card";
+
+export default function SignIn() {
+  return <div>SignIn</div>;
+}
