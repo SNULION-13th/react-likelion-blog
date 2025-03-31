b152948999c2d9c248bc3308de4b7a76ac24380c
jdnjsy <jdnjsyoo@snu.ac.kr>
2025-04-01 01:28:54 +0900

    signin page

--- src/routes/Home/index.jsx
+++ src/routes/Home/index.jsx
@@ -22,23 +22,23 @@ export default function Home() {
         <div className="mx-auto grid grid-cols-1 gap-y-4 md:grid-cols-2 lg:grid-cols-3 px-10 mt-10 lg:w-[950px] md:w-[640px] w-[320px]">
           {/* TODO: 검색 결과 포스트 만들기 */}
           {posts.map((post) => (
-            <div key={post.id} className="border rounded-xl p-4 shadow-md flex flex-col justify-between min-h-[180px]">
+            <div key={post.id} className="border rounded-xl p-4 shadow-md flex flex-col justify-between min-h-[240px]">
               <div>
-                <h2 className="font-bold text-lg mb-1">{post.title}</h2>
-                <p>className="text-sm text-gray-600 mb-2
+                <h2 className="font-bold text-lg mb-1 ml-4 text-left">{post.title}</h2>
+                <p className="text-sm text-gray-600 mb-2 ml-4 mt-4 text-left">
                   {post.author.username}
                 </p>
 
-                <div className="flex flex-wrap gap-1">
+                <div className="flex flex-wrap gap-1 ml-6 mt-14">
                   {post.tags.map((tag) => (
-                    <span key={tag.id} className="bg-amber-400 text-white text-xs px-2 py-0.5 rounded-full">
+                    <span key={tag.id} className="bg-orange-400 text-white text-xs px-2 py-0.5 rounded-full">
                       #{tag.content}
                     </span>
                   ))}
                 </div>
               </div>
 
-              <div className="lex items-center mt-4 text-sm text-red-500">
+              <div className="flex items-center mb-4 text-sm text-red-500 text-left">
                 ❤️ {post.like_users.length}
               </div>
             </div>
--- src/routes/SignIn/index.jsx
+++ src/routes/SignIn/index.jsx
@@ -1,8 +1,3 @@
-//TODO: 로그인 페이지 구현
-export default function SignIn() {
-  return <div>SignIn</div>;
-}
-
 // ⬇️추가
 import {
   Card,
@@ -15,3 +10,29 @@ import {
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import logo from "@/assets/logo.png";
+
+//TODO: 로그인 페이지 구현
+export default function SignIn() {
+  return (
+    <div className="min-h-screen  flex items-center justify-center">
+      <Card className="w-[350px] shadow-md">
+        <CardHeader className="flex items-center justify-center">
+          <div className="mb-2">
+            <img src={logo} alt="logo" className="h-30 w-auto"></img>
+          </div>
+          <CardTitle className="text-xl text-center">로그인</CardTitle>
+        </CardHeader>
+        <CardContent className="space-y-4">
+          <Input placeholder="아이디를 입력하세요" />
+          <Input type="password" placeholder="비밀번호를 입력하세요" />
+        </CardContent>
+        <CardFooter className="flex justify-between gap-4">
+          <Button className="w-full bg-[#8b8b8b]" variant="outline">로그인</Button>
+          <Button className="w-full bg-[#8b8b8b]" variant="outline">회원가입</Button>
+        </CardFooter>
+      </Card>
+    </div>
+  )
+}
+
+
