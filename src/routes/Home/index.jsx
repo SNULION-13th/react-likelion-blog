5d2471b551f7bd9dd7d68c47b7b37cfee48a9e1b
HYUCK <jaychoi2001@gmail.com>
2025-04-01 19:52:40 +0900

    week4 finished

--- src/routes/Home/index.jsx
+++ src/routes/Home/index.jsx
@@ -1,7 +1,23 @@
 import { posts } from "../../data/posts";
-
+import {
+  Card,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+  CardDescription,
+} from "@/components/ui/card";
 import { Header, Input } from "@/shared/components";
+
 export default function Home() {
+  const allTags = posts.reduce((acc, post) => {
+    return acc.concat(post.tags);
+  }, []);
+  const shuffledTags = [...allTags].sort(() => 0.5 - Math.random());
+  const selectedTags = [];
+  for (let i = 0; i < Math.min(7, shuffledTags.length); i++) {
+    selectedTags.push(shuffledTags[i]);
+  }
+
   return (
     <>
       <Header />
@@ -17,10 +33,45 @@ export default function Home() {
               placeholder="태그를 검색하세요"
             />
           </div>
+          <div className="mt-2 flex flex-wrap gap-1 justify-center px-4">
+            {selectedTags.map((tag) => (
+              <span
+                key={tag.id}
+                className="inline-block bg-orange-400 text-white text-xs font-semibold px-2.5 py-0.5 rounded"
+              >
+                #{tag.content}
+              </span>
+            ))}
+          </div>
         </div>
 
-        <div className="mx-auto grid grid-cols-1 gap-y-4 md:grid-cols-2 lg:grid-cols-3 px-10 mt-10 lg:w-[950px] md:w-[640px] w-[320px]">
-          {/* TODO: 검색 결과 포스트 만들기 */}
+        <div className="mx-auto grid grid-cols-1 gap-x-4 gap-y-6 md:grid-cols-2 lg:grid-cols-3 px-10 mt-10 lg:w-[950px] md:w-[640px] w-[320px]">
+          {posts.map((post) => (
+            <Card key={post.id} className="flex flex-col justify-between">
+              <CardHeader className="text-left">
+                <CardTitle>{post.title}</CardTitle>
+                <CardDescription className="italic text-gray-700 font-medium">
+                  {post.author.username}
+                </CardDescription>
+              </CardHeader>
+              <CardFooter className="flex flex-col items-start gap-2">
+                <div className="flex flex-wrap gap-1">
+                  {post.tags.map((tag) => (
+                    <span
+                      key={tag.id}
+                      className="inline-block bg-orange-400 text-white text-xs font-semibold px-2.5 py-0.5 rounded"
+                    >
+                      #{tag.content}
+                    </span>
+                  ))}
+                </div>
+                <span className="text-sm text-gray-500 flex items-center">
+                  <span className="text-red-500 ml-1">❤️</span>
+                  {post.like_users.length}
+                </span>
+              </CardFooter>
+            </Card>
+          ))}
         </div>
       </div>
     </>
--- src/routes/SignIn/index.jsx
+++ src/routes/SignIn/index.jsx
@@ -1,12 +1,65 @@
-//TODO: 로그인 페이지 구현
 import {
   Card,
-  CardContent,
   CardHeader,
-  CardTitle,
   CardFooter,
+  CardTitle,
+  CardContent,
+  CardDescription,
 } from "@/components/ui/card";
+import { Header, Input, Button } from "@/shared/components";
+import logoSrc from "@/assets/logo.png";
 
 export default function SignIn() {
-  return <div>SignIn</div>;
+  return (
+    <>
+      <Header />
+      <div className="flex items-center justify-center min-h-[calc(100vh-80px)] bg-gray-50 p-4">
+        <Card className="w-full max-w-lg min-h-auto">
+          <CardHeader className="items-center text-center">
+            <img
+              src={logoSrc}
+              alt="로고"
+              className="w-20 h-20 mb-4 rounded-full"
+            />
+            <CardTitle className="text-2xl">로그인</CardTitle>
+          </CardHeader>
+          <form>
+            <CardContent className="space-y-4">
+              <div className="space-y-1.5">
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="아이디를 입력하세요"
+                  required
+                />
+              </div>
+              <div className="space-y-1.5">
+                <Input
+                  id="password"
+                  type="password"
+                  placeholder="비밀번호를 입력하세요"
+                  required
+                />
+              </div>
+            </CardContent>
+            <CardFooter>
+              {/* Button에 hover가 있는데 왜 추가해야만 동작하는지는 모르겠어요*/}
+              <Button
+                type="button"
+                className="w-full text-gray-700 mx-1 hover:!bg-amber-400 hover:text-white"
+              >
+                로그인
+              </Button>
+              <Button
+                type="button"
+                className="w-full text-gray-700 mx-1 hover:!bg-amber-400 hover:text-white"
+              >
+                회원가입
+              </Button>
+            </CardFooter>
+          </form>
+        </Card>
+      </div>
+    </>
+  );
 }
