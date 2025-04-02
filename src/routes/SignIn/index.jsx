4b573df9bed4196692e050b3ace5998271a5f4a7
suhyun <seraphina0911@snu.ac.kr>
2025-04-02 09:58:28 +0900

    homework week4

--- src/routes/SignIn/index.jsx
+++ src/routes/SignIn/index.jsx
@@ -1,11 +1,4 @@
-import {
-  Card,
-  CardContent,
-  CardHeader,
-  CardTitle,
-  CardFooter,
-} from "@/components/ui/card";
-
+//TODO: 로그인 페이지 구현
 export default function SignIn() {
   return <div>SignIn</div>;
 }
