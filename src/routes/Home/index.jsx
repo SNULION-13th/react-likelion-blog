e4b92dccf27d27035b37490362108b53f5c9af24
cho58 <oke08034@snu.ac.kr>
2025-04-09 15:30:04 +0900

    hw5 submission

--- src/routes/Home/index.jsx
+++ src/routes/Home/index.jsx
@@ -3,40 +3,68 @@ import { Input, TagBadge, PostDialog } from "@/shared/components";
 import { getPosts, getTags, getPostById } from "@/shared/api";
 import { createPost } from "./api";
 import { useNavigate } from "react-router";
+import { useState, useEffect } from "react";
+import { useAuthMode } from "@/shared/context";
+import { Button } from "@/shared/components";
+import { Dialog, DialogTrigger } from "@/components/ui/dialog";
 
 //HINT: State
-const posts = [];
-const searchTags = [];
-const storedTags = [];
-
 export default function Home() {
   const navigate = useNavigate();
+
+  const [posts, setPosts] = useState([]);
+  const [searchTags, setSearchTags] = useState([]);
+  const [storedTags, setStoredTags] = useState([]);
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+
+  const { user, isLoggedIn } = useAuthMode();
 
   const fetchPosts = async () => {
     const posts = await getPosts();
     console.log("post fetch response", posts);
+    setPosts(posts);
   };
 
   const fetchTags = async () => {
     const tags = await getTags();
     console.log("tag fetch response", tags);
+    setSearchTags(tags);
+    setStoredTags(tags);
   };
 
   const handleSearchTagInputChange = (e) => {
     const { value } = e.target;
     console.log("search tag input change", value);
+    setSearchTags(
+      storedTags.filter((tag) => {
+        return tag.content.startsWith(value);
+      })
+    );
   };
 
   const handleCreatePost = async (post, author) => {
-    const createResponse = await createPost({
-      ...post,
-      author,
-    });
-    const newPost = await getPostById(createResponse.postId);
-    console.log("new post", newPost);
+    try {
+      const createResponse = await createPost({
+        ...post,
+        author,
+      });
+      const newPost = await getPostById(createResponse.postId);
+      console.log("new post", newPost);
+      fetchPosts();
+      fetchTags();
+    } catch (error) {
+      console.error(
+        "Post creation failed:",
+        error.response?.data || error.message
+      );
+    }
   };
 
   // TODO: 페이지 진입 시 최초 한 번만 태그와 게시글 정보들 불러오기
+  useEffect(() => {
+    fetchPosts();
+    fetchTags();
+  }, []);
 
   return (
     <div className="pb-20 pt-14">
@@ -45,7 +73,11 @@ export default function Home() {
           <h1 className="uppercase text-6xl text-black">my blog</h1>
         </div>
         <div className="w-[90vw] max-w-md flex justify-center">
-          <Input type="text" placeholder="태그를 검색하세요" />
+          <Input
+            type="text"
+            placeholder="태그를 검색하세요"
+            onChange={handleSearchTagInputChange}
+          />
         </div>
         <div className="flex mt-5 justify-center flex-wrap">
           {searchTags.map((tag) => {
@@ -72,6 +104,20 @@ export default function Home() {
       </div>
       {/* TODO: 로그인한 유저 정보가 있을 때에만 게시글 작성 버튼이 나타난다. */}
       {/* TODO: PostDialog 컴포넌트 구현 */}
+      {isLoggedIn && (
+        <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+          <DialogTrigger asChild>
+            <Button className="mt-15">작성</Button>
+          </DialogTrigger>
+          <PostDialog
+            isOpen={isDialogOpen}
+            onSubmit={(post) => {
+              handleCreatePost(post, user.username);
+              setIsDialogOpen(false);
+            }}
+          />
+        </Dialog>
+      )}
     </div>
   );
 }
