a215504dc6132cb25b5e5478213a6240c1ecf8e9
lili <suanchoi1102@snu.ac.kr>
2025-04-09 23:30:58 +0900

    5주차 과제

--- src/shared/context/userContext.jsx
+++ src/shared/context/userContext.jsx
@@ -1,1 +1,25 @@
 //TODO: 유저 정보 저장 컨텍스트 구현
+import { createContext, useState, useContext } from "react";
+
+const DEFAULT_CONTEXT_VALUE = {
+  user: null,
+  setUser: () => {},
+};
+
+const UserContext = createContext(DEFAULT_CONTEXT_VALUE);
+
+export const UserContextProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export const useUserContext = () => {
+  const { user, setUser } = useContext(UserContext);
+
+  return { user, setUser };
+};
