4609aaaf1ecba52cd6a6c5e38fb2d84cc2aec9e3
Imggaggu <ggaggu@snu.ac.kr>
2025-04-10 18:50:36 +0900

    [Fix] postDialog tag css

--- src/shared/components/TagInputBox.jsx
+++ src/shared/components/TagInputBox.jsx
@@ -30,7 +30,9 @@ export function TagInputBox() {
 
       <div className="flex gap-2 flex-wrap">
         {tags.map((tag, idx) => (
-          <TagBadge key={idx} tag={tag} /> 
+          <span class="inline-flex items-center justify-center rounded-md border px-2 py-0.5 text-xs w-fit whitespace-nowrap shrink-0 [&>svg]:size-3 gap-1 [&>svg]:pointer-events-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive transition-[color,box-shadow] overflow-hidden border-transparent text-primary-foreground [a&]:hover:bg-primary/90 m-1 bg-amber-500 font-bold">
+            {tag}
+          </span>
         ))}
       </div>
     </div>
